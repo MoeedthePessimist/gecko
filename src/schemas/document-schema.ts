9c616640055c5c8fd279d3548856a3d904371c02
MoeedthePessimist <moeedawan2121@gmail.com>
2025-09-12 13:28:19 +0500

    added documents related logic

--- src/schemas/document-schema.ts
+++ src/schemas/document-schema.ts
@@ -4,9 +4,9 @@ export const documentFormSchema = z.object({
   id: z.string().optional(),
   title: z.string().optional().default(""),
   category: z.string().optional().default(""),
-  startDate: z.date().optional().default(new Date()),
-  endDate: z.date().optional().default(new Date()),
-  expiryDate: z.date().optional().default(new Date()),
+  startDate: z.date().optional().default(new Date()).nullable(),
+  endDate: z.date().optional().default(new Date()).nullable(),
+  expiryDate: z.date().optional().default(new Date()).nullable(),
   fileName: z.string().optional().default(""),
   remarks: z.string().optional().default(""),
 });
