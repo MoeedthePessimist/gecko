162ce55cf4f5520416ad7ab435dc902291e0874f
MoeedThePessimist <moeedawan2121@gmail.com>
2025-11-26 12:03:28 +0500

    dummy commit

--- src/app/admin/leave-record/page.tsx
+++ src/app/admin/leave-record/page.tsx
@@ -1,8 +1,15 @@
+"use client";
+
+import useLeave from "@/hooks/use-leave";
 import { LeaveWithNecessaryFields } from "@/types/leave.type";
 import React, { useState } from "react";
 
 const LeaveRecordPage = () => {
   const [leaves, setLeaves] = useState<Array<LeaveWithNecessaryFields>>([]);
+
+  const {} = useLeave(setLeaves);
+
+  console.log(leaves, "leaves");
 
   return <div>LeaveRecordPage</div>;
 };
