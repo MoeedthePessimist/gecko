1327be248e0540c0e529af9af69cc1dfb49d140c
MoeedThePessimist <moeedawan2121@gmail.com>
2025-12-18 17:39:11 +0500

    added frontend code changes for company additional data module

--- src/hooks/use-company.tsx
+++ src/hooks/use-company.tsx
@@ -2,11 +2,47 @@ import { useTypedQuery } from "./use-query";
 import { QUERY_KEYS } from "@/constants/query-keys";
 import { useAuthContext } from "@/context/auth-context";
 import { getCompanyAdditionalData } from "@/api/company";
+import { useGlobalModal } from "@/context/error-context";
+import { useRouter } from "next/router";
+import { useQueryClient } from "@tanstack/react-query";
+import { CompanyFormInputs, companyFormSchema } from "@/schemas/company-schema";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
 
 const useCompany = () => {
   const { user } = useAuthContext();
+  const company = user?.company;
 
   const companyId = user?.company?.id || "";
+
+  const { showError, showSuccess } = useGlobalModal();
+  const router = useRouter();
+  const queryClient = useQueryClient();
+
+  const companyForm = useForm<CompanyFormInputs>({
+    resolver: zodResolver(companyFormSchema(true)),
+    defaultValues: {
+      name: company?.name || "",
+      entity: company?.entity || "",
+      industry: company?.industry || "",
+      telephone: company?.telephone || "",
+      address: company?.address || "",
+      organizationIdType: company?.organizationIdType || "",
+      csn: company?.csn || "",
+      uen: company?.uen || "",
+      logo: company?.logo || "",
+      paidLeaves: company?.paidLeaves ?? 24,
+      bank:
+        company?.bank && typeof company?.bank !== "string"
+          ? {
+              id: company?.bank?.id || "",
+              bankName: company?.bank?.bankName || "",
+              bankAccountNumber: company?.bank?.bankAccountNumber || "",
+              bankSwiftCode: company?.bank?.bankSwiftCode || "",
+            }
+          : undefined,
+    },
+  });
 
   const getCompanyAdditionalDataQuery = useTypedQuery({
     queryKey: [QUERY_KEYS.COMPANY_ADDITIONAL_DATA(companyId)],
---
+++ src/schemas/company-schema.ts
@@ -0,0 +1,22 @@
+import { z } from "zod";
+import { bankFormSchema } from "./bank-schema";
+
+export const companyFormSchema = (isUpdate?: boolean) =>
+  z.object({
+    name: z.string().nonempty("Company name is required"),
+    entity: z.string().nonempty("Please select company entity"),
+    industry: z.string().nonempty("Please select industry"),
+    telephone: z.string().nonempty("Telephone number is required"),
+    address: z.string().optional(),
+    organizationIdType: z.string().optional(),
+    csn: z.string().optional(),
+    uen: z.string().nonempty("UEN is required"),
+    logo: z.string().optional(),
+    paidLeaves: z
+      .number()
+      .min(0, "Paid leaves must be a positive number")
+      .default(24),
+    bank: bankFormSchema(isUpdate).optional(),
+  });
+
+export type CompanyFormInputs = z.infer<ReturnType<typeof companyFormSchema>>;
